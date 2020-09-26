28b1835e92e9324c0ce8e8060eaab61dd5e3d4a2
kshitiz-saxena <info@kshitizsaxena.com>
2020-09-27 01:43:27 +0530

    added skills in Profile

--- src/main/java/com/kshitizsaxena/profile/model/Profile.java
+++ src/main/java/com/kshitizsaxena/profile/model/Profile.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection = "profiles")
 @AllArgsConstructor
@@ -13,4 +15,5 @@ public class Profile {
     private String userId;
     private String firstName;
     private String lastName;
+    private List<String> skills;
 }

e1e43a77ca5a39a1b30dddb137babf10285c5187
kshitiz-saxena <info@kshitizsaxena.com>
2020-09-27 00:45:42 +0530

    added NoArgsConstructor

--- src/main/java/com/kshitizsaxena/profile/model/Profile.java
+++ src/main/java/com/kshitizsaxena/profile/model/Profile.java
@@ -2,11 +2,13 @@ package com.kshitizsaxena.profile.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "profiles")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
     private String userId;
     private String firstName;
